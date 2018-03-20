43210e3b5b932750cf2c00f7a4ae96427d5275e5
jarekkucharuk <jarekkucharuk@gmail.com>
2018-03-20 19:36:21 +0100

    turnOn & turnOff

--- src/test/java/com/demo/camera/PhotoCameraTest.java
+++ src/test/java/com/demo/camera/PhotoCameraTest.java
@@ -1,14 +1,26 @@
 package com.demo.camera;
-
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import static org.mockito.Mockito.mock;
 public class PhotoCameraTest {
 
-
     @Test
-    public void changeName() {
-        Assertions.assertThat(this).isNotNull();
+    public void turningOnCameraTurnsOnSensor() {
+
+        ImageSensor sensor=mock(ImageSensor.class);
+        PhotoCamera camera=new PhotoCamera(sensor);
+        camera.turnOn();
+        Mockito.verify(sensor).turnOn();
+    }
+    @Test
+    public void turningOffCameraTurnsOffSensor() {
+
+        ImageSensor sensor = mock(ImageSensor.class);
+        PhotoCamera camera = new PhotoCamera(sensor);
+        camera.turnOff();
+        Mockito.verify(sensor).turnOff();
     }
 
 }
